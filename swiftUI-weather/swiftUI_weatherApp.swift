86f4913cf4b640c00546b7f14bb9714194833896
Hadeel Jamoul <hadeelgamol@gmail.com>
2024-05-22 11:49:44 +0300

    Initial Commit

---
+++ swiftUI-weather/swiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_weatherApp.swift
+//  swiftUI-weather
+//
+//  Created by Hadeel Jamoul on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
